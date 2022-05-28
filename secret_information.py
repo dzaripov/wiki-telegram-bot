65afca1d415c6df8153620e1dcaf742d5bb7503f
Zaripov Danis <daniszaripov02@mail.ru>
2022-05-28 10:46:23 +0300

    Create secret_information.py

---
+++ secret_information.py
@@ -0,0 +1,5 @@
+my_token = '0000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' # Telegram bot token. See more: @botfather
+chat_ids = ['-10000', '-200000000'] # Chat ids in which notificatins will be sended
+mysql_database_wiki = 'name' # Name of MySQL wiki database
+mysql_password_wiki = 'password' # Password of MySQL wiki database
+sitename = 'example.com' # Domain name of site
