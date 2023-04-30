69f735dcffdcc6571175912a28a2de759a8b9c3c
dzaripov <daniszaripov02@mail.ru>
2023-04-30 23:37:54 +0300

    Added docstrings to every function

--- telegram_bot_wiki.py
+++ telegram_bot_wiki.py
@@ -1,7 +1,8 @@
 import telegram
 import time
 import logging
-from secret_information import my_token, chat_ids, host, user, password, database, sitename, time_sleep
+from secret_information import (my_token, chat_ids, host, user, 
+                                password, database, sitename, time_sleep)
 from mysql.connector import connect, Error
 
 
@@ -17,22 +18,39 @@ logging.basicConfig(filename='/var/log/wiki.log',
 
 def bold(text):
     """
-    Transfroms text to bold
+    Transform text to bold
+
+    Args:
+        text (str): The text to transform
+
+    Returns:
+        str: The bold text
     """
     return '<b>' + text + '</b>'
 
 
 def create_link(link, title):
     """
-    Telegram hyperlink creation
+    Create a Telegram hyperlink
+
+    Args:
+        link (str): The URL link
+        title (str): The text to display as the link
+
+    Returns:
+        str: The HTML code for the hyperlink
     """
     return f'<a href="{link}">{title}</a>'
 
 
 def send(msg, chat_id, token=my_token):
     """
-    Sends a message to a telegram user or group specified on chatId
-    chat_id must be a number!
+    Sends a message to a Telegram user or group specified by chat_id
+
+    Args:
+        msg (str): The message to send
+        chat_id (str): The ID of the chat to send the message to
+        token (str, optional): The API token for the Telegram bot. Defaults to my_token.
     """
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=msg, parse_mode='HTML')
@@ -40,7 +58,16 @@ def send(msg, chat_id, token=my_token):
 
 def create_message_wiki(rc_title, summary_comment, author_wiki, rc_cur_id):
     """
-    Creates a message to be sent to a telegram group or a user
+    Creates a message to be sent to a Telegram group or user
+
+    Args:
+        rc_title (str): The title of the wiki page that was changed
+        summary_comment (str): A summary of the changes made to the page
+        author_wiki (str): The username of the person who made the changes
+        rc_cur_id (int): The current revision ID of the page (ID of the change)
+
+    Returns:
+        str: The message to send
     """
     title = rc_title.replace('_', ' ')
     link = f'https://{sitename}/wiki/?curid={rc_cur_id}'
@@ -51,27 +78,37 @@ def create_message_wiki(rc_title, summary_comment, author_wiki, rc_cur_id):
 
 def get_activity_wiki(length_act=5):
     """
-    Get recent changes from wiki that dont minor (or page creation) and not posted earlier.
-    Get title, timestamp, id of change, id of page that contain change,
-    summary of change, name of author.
-    Return list of tuples.
+    Gets recent changes from the wiki that aren't minor 
+    (or page creation) and haven't been posted earlier.
+    Returns a list of tuples containing the title, timestamp, 
+    ID of the change, ID of the page that contains the change,
+    summary of the change, and name of the author.
+
+    Args:
+        length_act (int, optional): The number of recent changes to get. 
+        Defaults to 5.
+
+    Returns:
+        tuple: A tuple containing the list of recent changes 
+        and the range of recent changes to process.
     """
+
     query_get_activity = f"""
-    SELECT
-        rc_title, 
-        rc_timestamp, 
-        rc_id,
-        rc_cur_id,
-        comment_text,
-        actor_name
-     FROM recentchanges
-     LEFT JOIN comment ON rc_comment_id = comment_id
-     LEFT JOIN actor ON rc_actor = actor_id
-     WHERE rc_log_type is NULL and
-           rc_id NOT IN (SELECT posted_act_id 
-                     FROM recentchangesposted) and
-           rc_new = 0 and
-           rc_minor = 0"""
+        SELECT
+            rc_title, 
+            rc_timestamp, 
+            rc_id,
+            rc_cur_id,
+            comment_text,
+            actor_name
+         FROM recentchanges
+         LEFT JOIN comment ON rc_comment_id = comment_id
+         LEFT JOIN actor ON rc_actor = actor_id
+         WHERE rc_log_type is NULL and
+               rc_id NOT IN (SELECT posted_act_id 
+                         FROM recentchangesposted) and
+               rc_new = 0 and
+               rc_minor = 0"""
 
     with connect(**CONFIG) as cnx:
         with cnx.cursor(buffered=True) as cursor:
@@ -115,6 +152,12 @@ def post_if_new_activity_wiki():
 
 
 def is_db_created():
+    """
+    Checks if the recentchangesposted table exists in the database.
+
+    Returns:
+    bool: True if the table exists, False otherwise.
+    """
     with connect(**CONFIG) as cnx:
         with cnx.cursor(buffered=True) as cursor:
             cursor.execute('SHOW TABLES')
@@ -129,6 +172,10 @@ def is_db_created():
 
 
 def create_posted_activity_db():
+    """
+    Creates the recentchangesposted table in the database 
+    if it does not already exist.
+    """
     if is_db_created():
         logging.info('DB is already created!')
     else:
@@ -141,6 +188,10 @@ def create_posted_activity_db():
 
 
 def main():
+    """
+    The main function of the program that runs the 
+    function in a loop every time_sleep number of seconds.
+    """
     try:
         while True:
             post_if_new_activity_wiki()
